8822bf069bcc056aad1892fd1c7061002c6bf739
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-14 00:32:36 +0900

    [add] set NavHost [yuya]

--- app/src/main/java/com/example/eachinternshipschedule/MainActivity.kt
+++ app/src/main/java/com/example/eachinternshipschedule/MainActivity.kt
@@ -6,30 +6,40 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.eachinternshipschedule.ui.theme.EachInternshipScheduleTheme
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EachInternshipScheduleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navHost = rememberNavController()
 
+                    NavHost(
+                        navController = navHost,
+                        startDestination = Routes.ScheduleListScreen.route
+                    ) {
+                        composable(Routes.ScheduleListScreen.route) {}
+                        composable(Routes.PostScheduleScreen.route){}
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-sealed class Routes(route : String){
-    object PostScheduleScreen : Routes("postScheduleScreen")
-    object ScheduleListScreen : Routes("scheduleListScreen")
-}
+    sealed class Routes(val route: String) {
+        object PostScheduleScreen : Routes("postScheduleScreen")
+        object ScheduleListScreen : Routes("scheduleListScreen")
+    }
