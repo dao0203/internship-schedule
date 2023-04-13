c5f1c84114d7010fc9721f24504c2880ef1fa5e2
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-04-13 13:17:41 +0900

    [add] PostSchedule file to write post schedule screen (YuyaSato)

---
+++ app/src/main/java/com/example/eachinternshipschedule/view/post_schedule/PostSchedule.kt
@@ -0,0 +1,22 @@
+package com.example.eachinternshipschedule.view.post_schedule
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(name = "PostScheduleScreen")
+@Composable
+fun PostScheduleScreen() {
+    Column(
+        Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+    ) {
+
+    }
+}
