662e3630a0b3b202779ee9f931e4efe9bf7bf54f
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-16 14:34:50 +0900

    [add] post schedule ui state[yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/viewmodel/PostScheduleViewModel.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/viewmodel/PostScheduleViewModel.kt
@@ -4,19 +4,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.schedule.eachinternshipschedule.model.Schedule
+import com.schedule.eachinternshipschedule.model.ScheduleErrorMsg
+import com.schedule.eachinternshipschedule.model.ScheduleValid
 import com.schedule.eachinternshipschedule.repository.FirestoreRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-class PostScheduleViewModel(private val firestoreRepository: FirestoreRepository) :ViewModel(){
+class PostScheduleViewModel(private val firestoreRepository: FirestoreRepository) : ViewModel() {
 
-    fun insertSchedule(schedule: Schedule){
+    private val _uiState = MutableStateFlow(PostScheduleUiState())
+    fun insertSchedule(schedule: Schedule) {
         viewModelScope.launch {
             firestoreRepository.insertSchedule(schedule)
         }
     }
 }
 
-class PostScheduleModelFactory(private val repository: FirestoreRepository) :
+class PostScheduleViewModelFactory(private val repository: FirestoreRepository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PostScheduleViewModel::class.java)) {
@@ -26,3 +30,30 @@ class PostScheduleModelFactory(private val repository: FirestoreRepository) :
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
+
+data class PostScheduleUiState(
+    val isLoading: Boolean = false,
+    val routeExpanded : Boolean = false,
+    val statusExpanded : Boolean = false,
+    val schedule: Schedule = Schedule(
+        companyName = "",
+        internshipName = "",
+        date = "",
+        route = "",
+        routeStatus = ""
+    ),
+    val scheduleErrorMsg: ScheduleErrorMsg = ScheduleErrorMsg(
+        companyNameError = "",
+        internshipNameError = "",
+        dateError = "",
+        routeError = "",
+        routeStatusError = "",
+    ),
+    val scheduleValid: ScheduleValid = ScheduleValid(
+        isCompanyNameValid = false,
+        isInternshipName = false,
+        isDateValid = false,
+        isRouteStatusValid = false,
+        isRouteValid = false,
+    )
+)
