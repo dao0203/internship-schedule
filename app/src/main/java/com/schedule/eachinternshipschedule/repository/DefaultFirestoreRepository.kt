66c7458219ee523877c91d8cd012c3b7cf60036a
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-16 17:40:07 +0900

    [update] set function of get schedule data in firestore[yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/repository/DefaultFirestoreRepository.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/repository/DefaultFirestoreRepository.kt
@@ -4,13 +4,26 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.schedule.eachinternshipschedule.model.Schedule
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 
-class DefaultFirestoreRepository(private val firestore: FirebaseFirestore) : FirestoreRepository {
+class DefaultFirestoreRepository(firestore: FirebaseFirestore) : FirestoreRepository {
 
     private val scheduleRef = firestore.collection("schedule")
 
     override suspend fun getSchedule(): Flow<List<Schedule>> = flow {
-        return@flow
+        val scheduleList = mutableListOf<Schedule>()
+        val snapshot = scheduleRef.get().await()
+        for(document in snapshot.documents){
+            val schedule = Schedule(
+                companyName = document.getString("companyName") ?: "NoData",
+                internshipName = document.getString("internshipName") ?: "NoData",
+                date = document.getString("date") ?: "NoData",
+                route = document.getString("route") ?: "NoData",
+                routeStatus = document.getString("routeStatus") ?: "NoData"
+            )
+            scheduleList.add(schedule)
+        }
+        emit(scheduleList)
     }
 
     override suspend fun insertSchedule(schedule: Schedule) {
