ee4dd6c0c574250c39349d9eb6475d688552bec8
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-16 17:50:34 +0900

    [add] ScheduleListViewModelFactory [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/viewmodel/ScheduleListViewModel.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/viewmodel/ScheduleListViewModel.kt
@@ -8,6 +8,17 @@ class ScheduleListViewModel(private val repository: DefaultFirestoreRepository) 
 
 }
 
+class ScheduleListViewModelFactory(private val repository: FirestoreRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScheduleListViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ScheduleListViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
 sealed interface ScheduleListUiState {
     object Loading : ScheduleListUiState
     data class Success(val scheduleList: List<Schedule>) : ScheduleListUiState
