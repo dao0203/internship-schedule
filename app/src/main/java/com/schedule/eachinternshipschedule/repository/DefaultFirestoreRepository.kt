f3b91caf4b05893b492933db504ac3ed3a058317
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-16 18:11:13 +0900

    [add] change firestore data sort in order of date   [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/repository/DefaultFirestoreRepository.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/repository/DefaultFirestoreRepository.kt
@@ -1,6 +1,7 @@
 package com.schedule.eachinternshipschedule.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.schedule.eachinternshipschedule.model.Schedule
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -12,7 +13,8 @@ class DefaultFirestoreRepository(firestore: FirebaseFirestore) : FirestoreReposi
 
     override suspend fun getSchedule(): Flow<List<Schedule>> = flow {
         val scheduleList = mutableListOf<Schedule>()
-        val snapshot = scheduleRef.get().await()
+        //ドキュメントを実施日の日付順に並べる
+        val snapshot = scheduleRef.orderBy("date", Query.Direction.ASCENDING).get().await()
         for(document in snapshot.documents){
             val schedule = Schedule(
                 companyName = document.getString("companyName") ?: "NoData",
