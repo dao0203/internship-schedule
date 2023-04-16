19835febed46027b6e745236a6d0dc0546f06157
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-16 20:52:19 +0900

    [add] add file for uiState of loading [yuya]

---
+++ app/src/main/java/com/schedule/eachinternshipschedule/view/schedule_list/ScheduleListScrrenWhenLoading.kt
@@ -0,0 +1,14 @@
+package com.schedule.eachinternshipschedule.view.schedule_list
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScheduleListScreenWhenLoading(
+    modifier: Modifier = Modifier
+){
+    Column {
+
+    }
+}
