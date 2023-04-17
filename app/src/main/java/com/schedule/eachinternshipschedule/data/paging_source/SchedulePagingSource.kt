7a90f2af1e3c255255f7eb5c59256deb4b433956
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-18 03:00:15 +0900

    [update] unable to get each date order  [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/data/paging_source/SchedulePagingSource.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/data/paging_source/SchedulePagingSource.kt
@@ -1,5 +1,39 @@
 package com.schedule.eachinternshipschedule.data.paging_source
 
-class SchedulePagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import com.schedule.eachinternshipschedule.model.Schedule
+import kotlinx.coroutines.tasks.await
+
+class SchedulePagingSource(firestore: FirebaseFirestore) : PagingSource<QuerySnapshot, Schedule>() {
+    private val scheduleRef = firestore.collection("schedule")
+
+    override fun getRefreshKey(state: PagingState<QuerySnapshot, Schedule>): QuerySnapshot? {
+        return null
+    }
+
+    override suspend fun load(params: LoadParams<QuerySnapshot>): LoadResult<QuerySnapshot, Schedule> {
+        return try {
+            val currentPage = params.key ?: scheduleRef
+                .orderBy("date", Query.Direction.ASCENDING)
+                .get()
+                .await()
+            val lastVisibleProduct = currentPage.documents[currentPage.size() - 1]
+            val nextPage = scheduleRef
+                .orderBy("date", Query.Direction.ASCENDING)
+                .startAfter(lastVisibleProduct)
+                .get()
+                .await()
+            LoadResult.Page(
+                data = currentPage.toObjects(Schedule::class.java),
+                prevKey = null,
+                nextKey = nextPage
+            )
+        }catch (e: Exception){
+            LoadResult.Error(e)}
+    }
 
 }
