80f0110f250575afbf61b32165942a59118b95bf
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-18 03:32:15 +0900

    [update] update function of argument  [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/data/repository/DefaultFirestoreRepository.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/data/repository/DefaultFirestoreRepository.kt
@@ -1,31 +1,15 @@
 package com.schedule.eachinternshipschedule.data.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
+import com.schedule.eachinternshipschedule.data.paging_source.SchedulePagingSource
 import com.schedule.eachinternshipschedule.model.Schedule
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.tasks.await
 
-class DefaultFirestoreRepository(firestore: FirebaseFirestore) : FirestoreRepository {
+class DefaultFirestoreRepository(val firestore: FirebaseFirestore) : FirestoreRepository {
 
     private val scheduleRef = firestore.collection("schedule")
 
-    override suspend fun getSchedule(): Flow<List<Schedule>> = flow {
-        val scheduleList = mutableListOf<Schedule>()
-        //ドキュメントを実施日の日付順に並べる
-        val snapshot = scheduleRef.orderBy("date", Query.Direction.ASCENDING).get().await()
-        for(document in snapshot.documents){
-            val schedule = Schedule(
-                companyName = document.getString("companyName") ?: "NoData",
-                internshipName = document.getString("internshipName") ?: "NoData",
-                date = document.getString("date") ?: "NoData",
-                route = document.getString("route") ?: "NoData",
-                routeStatus = document.getString("routeStatus") ?: "NoData"
-            )
-            scheduleList.add(schedule)
-        }
-        emit(scheduleList)
+    override fun getSchedule(): SchedulePagingSource {
+        return SchedulePagingSource(firestore)
     }
 
     override suspend fun insertSchedule(schedule: Schedule) {
