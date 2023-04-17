319215a1c32bb3bf5dfad2f1ce29596824f87827
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-17 23:30:57 +0900

    [clean] merge into one file [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/view/schedule_list/ScheduleListScreen.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/view/schedule_list/ScheduleListScreen.kt
@@ -1,18 +1,31 @@
 package com.schedule.eachinternshipschedule.view.schedule_list
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.firestore.FirebaseFirestore
+import com.schedule.eachinternshipschedule.model.Schedule
 import com.schedule.eachinternshipschedule.repository.DefaultFirestoreRepository
 import com.schedule.eachinternshipschedule.viewmodel.ScheduleListUiState
 import com.schedule.eachinternshipschedule.viewmodel.ScheduleListViewModel
@@ -47,14 +60,70 @@ fun ScheduleListScreen(
                     ScheduleListScreenWhenLoading()
                 }
                 is ScheduleListUiState.Success -> {
-                    ScheduleListScreenWhenSuccess(
-                        scheduleList = (uiState as ScheduleListUiState.Success).scheduleList,
+                    LazyColumn(
+                        state = rememberLazyListState(),
+                        contentPadding = PaddingValues(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
                     )
+                    {
+                        items((uiState as ScheduleListUiState.Success).scheduleList.size) { schedule ->
+                            ScheduleItem(schedule = (uiState as ScheduleListUiState.Success).scheduleList[schedule])
+                        }
+                    }
                 }
-                is ScheduleListUiState.Error -> {}
+                is ScheduleListUiState.Error -> {
+
+                }
             }
         }
 
     }
+}
 
+@Composable
+fun ScheduleListScreenWhenLoading(
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        CircularProgressIndicator()
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun ScheduleItem(
+    schedule: Schedule,
+) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        onClick = {
+
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                text = schedule.companyName,
+            )
+            Text(
+                text = schedule.internshipName,
+            )
+            Text(
+                text = schedule.date,
+            )
+            Text(
+                text = schedule.route,
+            )
+            Text(
+                text = schedule.routeStatus,
+            )
+        }
+    }
 }
