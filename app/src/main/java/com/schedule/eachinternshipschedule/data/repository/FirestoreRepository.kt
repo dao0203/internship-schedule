6aa2dd619296a9d39589d0569f6c8cbbf4ca0a86
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-18 03:30:52 +0900

    [update] change function  [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/data/repository/FirestoreRepository.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/data/repository/FirestoreRepository.kt
@@ -1,10 +1,9 @@
 package com.schedule.eachinternshipschedule.data.repository
 
+import com.schedule.eachinternshipschedule.data.paging_source.SchedulePagingSource
 import com.schedule.eachinternshipschedule.model.Schedule
-import kotlinx.coroutines.flow.Flow
-
 
 interface FirestoreRepository {
     suspend fun insertSchedule(schedule: Schedule)
-    suspend fun getSchedule() : Flow<List<Schedule>>
+    fun getSchedule(): SchedulePagingSource
 }
