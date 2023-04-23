6bcb73538e3e9d2d101de919c29af376c51d8f45
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-24 00:30:31 +0900

    [update] change paging source argument to query [yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/data/SchedulePagingSource.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/data/SchedulePagingSource.kt
@@ -2,15 +2,13 @@ package com.schedule.eachinternshipschedule.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 import com.schedule.eachinternshipschedule.utils.Constants
 import com.schedule.eachinternshipschedule.model.Schedule
 import kotlinx.coroutines.tasks.await
 
-class SchedulePagingSource(firestore: FirebaseFirestore) : PagingSource<QuerySnapshot, Schedule>() {
-    private val scheduleRef = firestore.collection("schedule")
+class SchedulePagingSource(private val query: Query) : PagingSource<QuerySnapshot, Schedule>() {
 
     override fun getRefreshKey(state: PagingState<QuerySnapshot, Schedule>): QuerySnapshot? {
         return null
@@ -19,16 +17,12 @@ class SchedulePagingSource(firestore: FirebaseFirestore) : PagingSource<QuerySna
     override suspend fun load(params: LoadParams<QuerySnapshot>): LoadResult<QuerySnapshot, Schedule> {
         return try {
 
-            val currentPage = params.key ?: scheduleRef
-                .orderBy("date", Query.Direction.ASCENDING)
-
+            val currentPage = params.key ?: query
                 .limit(Constants.PAGE_SIZE.toLong())
                 .get()
                 .await()
             val lastVisibleProduct = currentPage.documents[currentPage.size() - 1]
-            val nextPage = scheduleRef
-                .orderBy("date", Query.Direction.ASCENDING)
-
+            val nextPage =query
                 .limit(Constants.PAGE_SIZE.toLong())
                 .startAfter(lastVisibleProduct)
                 .get()
