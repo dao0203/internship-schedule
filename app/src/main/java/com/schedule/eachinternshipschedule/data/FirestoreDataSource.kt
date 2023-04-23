404c987439a6a074c697dc2d09a7cade6f9c4833
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-24 00:29:11 +0900

    [add] FirestoreDataSource [yuya]

---
+++ app/src/main/java/com/schedule/eachinternshipschedule/data/FirestoreDataSource.kt
@@ -0,0 +1,21 @@
+package com.schedule.eachinternshipschedule.data
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.schedule.eachinternshipschedule.model.Schedule
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FirestoreDataSource @Inject constructor(
+    firestore: FirebaseFirestore
+) {
+
+    private val scheduleRef = firestore.collection("schedule")
+
+    fun getScheduleQuery() = scheduleRef.orderBy("date", Query.Direction.ASCENDING)
+
+    fun insertSchedule(schedule: Schedule) {
+        scheduleRef.add(schedule)
+    }
+}
