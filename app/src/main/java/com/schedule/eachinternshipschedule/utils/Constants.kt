101d1fb9aeb06c29bd9a8c0d0f39c6f117a83dd6
佐藤 佑哉 <y-sato@sstn.jp>
2023-04-26 13:02:26 +0900

    [add] drop down menu items in constants class[yuya]

--- app/src/main/java/com/schedule/eachinternshipschedule/utils/Constants.kt
+++ app/src/main/java/com/schedule/eachinternshipschedule/utils/Constants.kt
@@ -2,4 +2,18 @@ package com.schedule.eachinternshipschedule.utils
 
 object Constants {
     const val PAGE_SIZE = 20
+    val ROUTE_ITEMS: List<String> = listOf(
+        "選考を選択してください",
+        "書類",
+        "一次面接",
+        "二次面接",
+        "三次面接",
+        "最終面接",
+    )
+    val STATUS_ITEMS: List<String> = listOf(
+        "選考状況を選択してください",
+        "実施日(提出)",
+        "選考中",
+        "通過",
+    )
 }
