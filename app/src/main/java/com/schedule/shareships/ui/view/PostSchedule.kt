64c345bcfd3656fddf422080911326d7f2c51c00
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-03 14:14:00 +0900

    [add] set DatePickerDialog [yuya]

--- app/src/main/java/com/schedule/shareships/ui/view/PostSchedule.kt
+++ app/src/main/java/com/schedule/shareships/ui/view/PostSchedule.kt
@@ -1,11 +1,15 @@
 package com.schedule.shareships.ui.view
 
+import android.app.DatePickerDialog
+import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,6 +17,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -129,6 +134,30 @@ fun PostScheduleScreen(
                     if (textFieldErrorUiState.isDateInvalid) {
                         Text(text = Constants.INPUT_ERROR_MSG)
                     }
+                },
+                readOnly = true,
+                trailingIcon = {
+                    IconButton(
+                        onClick = {
+                            showDatePickerDialog(
+                                context = context,
+                                onDateSet = {year, month, dayOfMonth ->
+                                    viewModel.onDateValueChange(
+                                        year = year,
+                                        month = month,
+                                        dayOfMonth = dayOfMonth
+                                    )
+                                }
+                            )
+                        },
+                        content = {
+                            Icon(
+                                //カレンダーのアイコン
+                                imageVector = Icons.Default.DateRange,
+                                contentDescription = null,
+                            )
+                        },
+                    )
                 }
             )
             Spacer(
