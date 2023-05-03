56de7655a8e4b878884b5d1a947e9eb786f0c562
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-03 23:54:41 +0900

    [update] enable to scroll  [yuya]

--- app/src/main/java/com/schedule/shareships/ui/view/PostSchedule.kt
+++ app/src/main/java/com/schedule/shareships/ui/view/PostSchedule.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
@@ -99,7 +101,8 @@ fun PostScheduleScreen(
         Column(
             modifier = Modifier
                 .padding(it)
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
