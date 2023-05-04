9cd3233af07d37db12b4ab0514c84bd734f5d2e3
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-04 15:29:59 +0900

    [update] change colorScheme [yuya]

--- app/src/main/java/com/schedule/shareships/ui/theme/Theme.kt
+++ app/src/main/java/com/schedule/shareships/ui/theme/Theme.kt
@@ -47,7 +47,7 @@ fun EachInternshipScheduleTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) DarkColorScheme else LightColorScheme
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
