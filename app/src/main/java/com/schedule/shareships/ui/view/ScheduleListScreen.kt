cc0b683fbe378b52d48842338087d5f1c98d95c7
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-04 19:01:21 +0900

    [add] remove scaffold  [yuya]

--- app/src/main/java/com/schedule/shareships/ui/view/ScheduleListScreen.kt
+++ app/src/main/java/com/schedule/shareships/ui/view/ScheduleListScreen.kt
@@ -1,5 +1,6 @@
 package com.schedule.shareships.ui.view
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,63 +12,32 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.schedule.shareships.model.Schedule
 import com.schedule.shareships.ui.viewmodel.ScheduleListViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.schedule.shareships.Routes
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun ScheduleListScreen(
     modifier: Modifier = Modifier,
-    navController: NavController = rememberNavController(),
+    navController: NavController = rememberAnimatedNavController(),
     scheduleListViewModel: ScheduleListViewModel = hiltViewModel()
 ) {
     val schedules: LazyPagingItems<Schedule> =
         scheduleListViewModel.items.collectAsLazyPagingItems()
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "スケジュール一覧",
-                        color = MaterialTheme.colorScheme.onPrimary,
-                    )
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(MaterialTheme.colorScheme.primary),
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    navController.navigate(Routes.PostScheduleScreen.route)
-                },
-                containerColor = MaterialTheme.colorScheme.primary,
-            ) {
-                Text(
-                    text = "投稿",
-                    color = MaterialTheme.colorScheme.onPrimary,
-                )
-            }
-        },
-    ) {
         Box(
-            modifier = modifier.padding(it),
+            modifier = modifier,
         ) {
             LazyColumn(
                 modifier = Modifier
@@ -107,7 +77,6 @@ fun ScheduleListScreen(
             }
         }
     }
-}
 
 @Composable
 fun ScheduleListScreenWhenLoading(
