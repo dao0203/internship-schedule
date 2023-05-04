7827ea518c7bb9b294e5808a731f9ca4865b84b0
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-04 17:43:27 +0900

    [add] original AppBar composable function [yuya]

--- app/src/main/java/com/schedule/shareships/EachInternshipScheduleApp.kt
+++ app/src/main/java/com/schedule/shareships/EachInternshipScheduleApp.kt
@@ -1,6 +1,47 @@
 package com.schedule.shareships
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+enum class Routes(val route: String, val title: String) {
+    PostScheduleScreen("postScheduleScreen", "スケジュール投稿"),
+    ScheduleListScreen("scheduleListScreen", "スケジュール一覧"),
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EachInternshipScheduleAppBar(
+    currentRoute: Routes,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = currentRoute.title,
+            )
+        },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "戻る",
+                    )
+                }
+            }
+        },
+    )
+}
 
 @Composable
 fun EachInternshipScheduleApp() {
