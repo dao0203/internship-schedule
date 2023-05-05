aa268282fe2b331177052b918a5f87699230b3a5
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-06 00:46:18 +0900

    [add] change class name  [yuya]

--- app/src/main/java/com/schedule/shareships/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/schedule/shareships/ui/viewmodel/LoginViewModel.kt
@@ -8,12 +8,12 @@ import kotlinx.coroutines.flow.asStateFlow
 
 @HiltViewModel
 class LoginViewModel @Inject constructor() : ViewModel() {
-    private val _loginTextFieldErrorState = MutableStateFlow(LoginTextFieldErrorState())
+    private val _loginTextFieldErrorState = MutableStateFlow(LoginTextFieldErrorUiState())
     val loginTextFieldErrorState = _loginTextFieldErrorState.asStateFlow()
 
 }
 
-data class LoginTextFieldErrorState(
+data class LoginTextFieldErrorUiState(
     val isMailAddressInValid: Boolean = false,
     val isPasswordInValid: Boolean = false
 )
