385f299507a06590526485ef48e41d8529b9a414
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-06 15:16:14 +0900

    [add] on value change function [yuya]

--- app/src/main/java/com/schedule/shareships/ui/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/schedule/shareships/ui/viewmodel/RegisterViewModel.kt
@@ -2,11 +2,82 @@ package com.schedule.shareships.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.schedule.shareships.model.RegisterData
+import com.schedule.shareships.utils.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor() : ViewModel() {
+    private val _registerUiState = MutableStateFlow(RegisterUiState())
+    val registerUiState = _registerUiState.asStateFlow()
+
+    //Emailのテキストを管理するメソッド
+    fun onEmailValueChange(completedText: String) {
+        _registerUiState.value = _registerUiState.value.copy(
+            registerData = _registerUiState.value.registerData.copy(
+                email = completedText
+            )
+        )
+    }
+
+    //UserNameのテキストを管理するメソッド
+    fun onUserNameValueChange(completedText: String) {
+        _registerUiState.value = _registerUiState.value.copy(
+            registerData = _registerUiState.value.registerData.copy(
+                userName = completedText
+            )
+        )
+    }
+
+    //GithubIdのテキストを管理するメソッド
+    fun onGithubIdValueChange(completedText: String) {
+        _registerUiState.value = _registerUiState.value.copy(
+            registerData = _registerUiState.value.registerData.copy(
+                githubId = completedText
+            )
+        )
+    }
+
+    //Passwordのテキストを管理するメソッド
+    fun onPasswordValueChange(completedText: String) {
+        _registerUiState.value = _registerUiState.value.copy(
+            registerData = _registerUiState.value.registerData.copy(
+                password = completedText
+            )
+        )
+    }
+
+    //再入力Passwordのテキストを管理するメソッド
+    fun onReenteredPasswordValueChange(completedText: String) {
+        _registerUiState.value = _registerUiState.value.copy(
+            reenteredPassword = completedText
+        )
+    }
+
+    //バリデーションを行うメソッド
+//    fun validateForm(): Boolean{
+//        val email = registerUiState.value.registerData.email
+//        val userName = registerUiState.value.registerData.userName
+//        val githubId = registerUiState.value.registerData.githubId
+//        val password = registerUiState.value.registerData.password
+//        val reenteredPassword = registerUiState.value.reenteredPassword
+//        //Emailのバリデーション
+//        if (email.isEmpty()) {
+//            _registerUiState.value = _registerUiState.value.copy(
+//                isEmailError = true,
+//                EmailErrorText = Constants.INPUT_ERROR_MSG
+//            )
+//        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            _registerUiState.value = _registerUiState.value.copy(
+//                isEmailError = true,
+//                EmailErrorText = Constants.EMAIL_INVALID_ERROR_MSG
+//            )
+//        }
+//        return true
+//    }
+
 }
 
 data class RegisterUiState(
