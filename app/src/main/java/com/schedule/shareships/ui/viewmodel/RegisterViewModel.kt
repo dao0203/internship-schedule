f6e92c1d5abb95cc00b896e03136ccf532ce5aa0
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-06 15:00:26 +0900

    [update] register screen ui state [yuya]

--- app/src/main/java/com/schedule/shareships/ui/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/schedule/shareships/ui/viewmodel/RegisterViewModel.kt
@@ -1,6 +1,7 @@
 package com.schedule.shareships.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.schedule.shareships.model.RegisterData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -8,3 +9,19 @@ import javax.inject.Inject
 class RegisterViewModel @Inject constructor() : ViewModel() {
 }
 
+data class RegisterUiState(
+    val registerData: RegisterData = RegisterData(
+        email = "",
+        userName = "",
+        githubId = "",
+        password = ""
+    ),
+    val isEmailError: Boolean = false,
+    val isUserNameError: Boolean = false,
+    val isGithubIdError: Boolean = false,
+    val isPasswordError: Boolean = false,
+    val EmailErrorText: String = "",
+    val UserNameErrorText: String = "",
+    val GithubIdErrorText: String = "",
+    val PasswordErrorText: String = "",
+)
