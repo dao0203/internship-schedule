f24e434747b2c0345565b2cf39d02761d7b8679e
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-06 15:29:30 +0900

    [add] validation from function in RegisterViewModel class [yuya]

--- app/src/main/java/com/schedule/shareships/ui/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/schedule/shareships/ui/viewmodel/RegisterViewModel.kt
@@ -1,5 +1,6 @@
 package com.schedule.shareships.ui.viewmodel
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
 import com.schedule.shareships.model.RegisterData
 import com.schedule.shareships.utils.Constants
@@ -57,26 +58,67 @@ class RegisterViewModel @Inject constructor() : ViewModel() {
     }
 
     //バリデーションを行うメソッド
-//    fun validateForm(): Boolean{
-//        val email = registerUiState.value.registerData.email
-//        val userName = registerUiState.value.registerData.userName
-//        val githubId = registerUiState.value.registerData.githubId
-//        val password = registerUiState.value.registerData.password
-//        val reenteredPassword = registerUiState.value.reenteredPassword
-//        //Emailのバリデーション
-//        if (email.isEmpty()) {
-//            _registerUiState.value = _registerUiState.value.copy(
-//                isEmailError = true,
-//                EmailErrorText = Constants.INPUT_ERROR_MSG
-//            )
-//        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-//            _registerUiState.value = _registerUiState.value.copy(
-//                isEmailError = true,
-//                EmailErrorText = Constants.EMAIL_INVALID_ERROR_MSG
-//            )
-//        }
-//        return true
-//    }
+    fun validateForm(): Boolean {
+        val email = registerUiState.value.registerData.email
+        val userName = registerUiState.value.registerData.userName
+        val githubId = registerUiState.value.registerData.githubId
+        val password = registerUiState.value.registerData.password
+        val reenteredPassword = registerUiState.value.reenteredPassword
+        //Emailのバリデーション
+        if (email.isEmpty()) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isEmailError = true,
+                EmailErrorText = Constants.INPUT_ERROR_MSG
+            )
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isEmailError = true,
+                EmailErrorText = Constants.EMAIL_INVALID_ERROR_MSG
+            )
+        }
+        //UserNameのバリデーション
+        if (userName.isEmpty()) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isUserNameError = true,
+                UserNameErrorText = Constants.INPUT_ERROR_MSG
+            )
+        }
+        //GithubIdのバリデーション
+        if (githubId.isEmpty()) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isGithubIdError = true,
+                GithubIdErrorText = Constants.INPUT_ERROR_MSG
+            )
+        }
+        //Passwordのバリデーション
+        if (password.isEmpty()) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isPasswordError = true,
+                PasswordErrorText = Constants.INPUT_ERROR_MSG
+            )
+        }
+        //再入力Passwordのバリデーション
+        if (reenteredPassword.isEmpty()) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isReenteredPasswordError = true,
+                ReenteredPasswordErrorText = Constants.INPUT_ERROR_MSG
+            )
+        } else if (password != reenteredPassword) {
+            _registerUiState.value = _registerUiState.value.copy(
+                isReenteredPasswordError = true,
+                ReenteredPasswordErrorText = Constants.PASSWORD_NOT_MATCH_ERROR_MSG
+            )
+        }
+
+        //バリデーション結果を返す
+        return registerUiState.value.run {
+            !isEmailError &&
+                    !isUserNameError &&
+                    !isGithubIdError &&
+                    !isPasswordError &&
+                    !isReenteredPasswordError
+        }
+    }
 
 }
 
