50e2c0594664760c6b8a387af6cb8cbbe8a9c39f
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-07 17:02:54 +0900

    [update] add firebase auth instance and change firestore instance to ktx.firestore  [yuya]

--- app/src/main/java/com/schedule/shareships/di/FirebaseModule.kt
+++ app/src/main/java/com/schedule/shareships/di/FirebaseModule.kt
@@ -1,6 +1,8 @@
 package com.schedule.shareships.di
 
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,5 +15,9 @@ object FirebaseModule {
 
     @Provides
     @Singleton
-    fun provideFirebaseFirestoreInstance() = FirebaseFirestore.getInstance()
+    fun provideFirebaseFirestoreInstance() = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuthInstance() = Firebase.auth
 }
