2c07c1669eed3d01802f7dbf7e820b65191c2005
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-16 02:17:22 +0900

    [add] add interface firestore data source[yuya]

---
+++ app/src/main/java/com/schedule/shareships/data/source/FirestoreDataSourceImpl.kt
@@ -0,0 +1,37 @@
+package com.schedule.shareships.data.source
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.schedule.shareships.model.Schedule
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FirestoreDataSourceImpl @Inject constructor(
+    firestore: FirebaseFirestore
+) : FirestoreDataSource{
+
+    private val scheduleRef = firestore.collection("schedule")
+
+    override fun getSchedulesQuery() = scheduleRef.orderBy("date", Query.Direction.ASCENDING)
+
+    override suspend fun getUserData(email: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getFollowedUsersQuery(email: String): Query {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getFollowedUsersSchedulesQuery(email: String): Query {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun followUser(email: String, followedUserEmail: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertSchedule(schedule: Schedule) {
+        scheduleRef.add(schedule)
+    }
+}
