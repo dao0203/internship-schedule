19f9306b9a968403aad3265d84c50bcfd3b7f0ff
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-30 03:07:53 +0900

    [add] 投稿スケジュールからモデルに変えるための静的メソッドを作成

--- app/src/main/java/com/schedule/shareships/data/models/ScheduleModel.kt
+++ app/src/main/java/com/schedule/shareships/data/models/ScheduleModel.kt
@@ -1,8 +1,10 @@
 package com.schedule.shareships.data.models
 
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
 
 data class ScheduleModel(
+    @DocumentId val id: String,
     val companyName: String,
     val internshipName: String,
     val date: Timestamp,
@@ -10,4 +12,27 @@ data class ScheduleModel(
     val routeStatus: String,
     val createdAt: Timestamp,
     val updatedAt: Timestamp
-)
+) {
+    companion object {
+        fun fromPostScheduleData(
+            companyName: String,
+            internshipName: String,
+            date: Timestamp,
+            route: String,
+            routeStatus: String,
+            createdAt: Timestamp,
+            updatedAt: Timestamp
+        ): ScheduleModel {
+            return ScheduleModel(
+                id = "",
+                companyName = companyName,
+                internshipName = internshipName,
+                date = date,
+                route = route,
+                routeStatus = routeStatus,
+                createdAt = createdAt,
+                updatedAt = updatedAt
+            )
+        }
+    }
+}
