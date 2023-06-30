07618a116ed562c8f1d93e1f8d3a8ecac8f4e465
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-30 12:35:49 +0900

    [delete] 不要なサービスクラスを削除

--- app/src/main/java/com/schedule/shareships/data/sources/AccountService.kt
+++
@@ -1,9 +0,0 @@
-package com.schedule.shareships.data.sources
-
-interface AccountService {
-    fun getCurrentUser(): String?
-    fun signIn(email: String, password: String)
-    fun signUp(email: String, password: String)
-    fun signOut()
-    fun isSignedIn(): Boolean
-}
